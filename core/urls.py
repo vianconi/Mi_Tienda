5189bfd1148e03b1041c5f535cdf617c5e96e411
user <emiliovianconi@gmail.com>
2025-04-04 16:42:03 -0300

    fifth commit

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 from products.views import ProductListView
@@ -14,3 +16,6 @@ urlpatterns = [
     path('usuarios/registro/', views.register, name='register'),
     path('productos/', include('products.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -4,17 +4,16 @@ from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
 
-# Create your models here.
+
 class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
     slug = models.SlugField(null=False, blank=False, unique=True)
+    image = models.ImageField(upload_to='products/', null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    #def save(self, *args, **kwargs):
-    #    self.slug = slugify(self.title)
-    #    super(Product, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
